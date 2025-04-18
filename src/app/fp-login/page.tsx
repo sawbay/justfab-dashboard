cdfc85df5be4470dadef847245199d5f4b9d7ca4
phatln <phatln@undercurrent.tech>
2025-04-18 15:43:02 +0700

    add logout

--- src/app/fp-login/page.tsx
+++ src/app/fp-login/page.tsx
@@ -1,9 +1,34 @@
 "use client";
 
+import { useAuth } from '@futureverse/auth-react';
 import { useAuthUi } from '@futureverse/auth-ui';
 
 export default function FPLoginPage() {
-    const { openLogin, closeLogin, isLoginOpen } = useAuthUi();
+  const { userSession } = useAuth();
+  const { openLogin } = useAuthUi();
 
-    return <button onClick={() => openLogin()}>Login</button>;
+  function signOutPass() {
+    throw new Error('Function not implemented.');
+  }
+
+  return <>
+    {userSession ? (
+      <>
+        <div className="row">User EOA: {userSession.eoa}</div>
+        <div className="row">User Chain ID: {userSession.chainId}</div>
+        <div className="row">User Custodian: {userSession.custodian}</div>
+        <div className="row">User Futurepass: {userSession.futurepass}</div>
+        <button
+          onClick={() => {
+            signOutPass();
+          }}
+          className="green"
+        >
+          Log Out
+        </button>
+      </>
+    ) : (
+      <button onClick={() => openLogin()}>Login</button>
+    )}
+  </>
 }
