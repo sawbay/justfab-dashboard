9ea5c35f8a25b3b679a3114b6bafd4bd8e583fda
phatln <phatln@undercurrent.tech>
2025-04-18 16:57:28 +0700

    try out

--- src/app/fp/page.tsx
+++ src/app/fp/page.tsx
@@ -1,39 +1,88 @@
 "use client";
 
 import { useAuth } from '@futureverse/auth-react';
-import { useEffect } from 'react';
+import { CSSProperties, useEffect, useState } from 'react';
 import { useRouter } from "next/navigation";
+import { UserSession } from '@futureverse/auth-react/auth';
+import { useAuthUi } from '@futureverse/auth-ui';
 
 export default function FPPage() {
-  const { userSession, signOutPass } = useAuth();
+  const { authClient } = useAuth();
+  const [signInState, setSignInState] = useState<boolean | undefined>(
+    undefined
+  );
+
   const router = useRouter();
 
   useEffect(() => {
-    if (userSession) {
-      console.log('User session:', userSession);
-    } else {
-      router.push("/fp/login");
-    }
-  }, [userSession]);
+    const userStateChange = (user: UserSession | undefined) => {
+      if (user) {
+        setSignInState(true);
+        router.push('/');
+      }
+      if (!user) {
+        setSignInState(false);
+      }
+    };
 
-  return <>
-    {userSession ? (
-      <>
-        <div className="row">User EOA: {userSession.eoa}</div>
-        <div className="row">User Chain ID: {userSession.chainId}</div>
-        <div className="row">User Custodian: {userSession.custodian}</div>
-        <div className="row">User Futurepass: {userSession.futurepass}</div>
-        <button
-          onClick={() => {
-            signOutPass();
+    authClient.addUserStateListener(userStateChange);
+    return () => {
+      authClient.removeUserStateListener(userStateChange);
+    };
+  }, [authClient, router]);
+
+  if (signInState === true) {
+    return (
+      <RowComponent showSpinner={true}>
+        Redirecting, please wait...
+      </RowComponent>
+    );
+  }
+
+  if (signInState === false) {
+    return (
+      <RowComponent showSpinner={false}>
+        <div>Not Authenticated - Please Log In...</div>
+        <LogIn
+          styles={{
+            padding: ' 16px',
+            fontWeight: '700',
+            fontSize: '1.2rem',
           }}
-          className="green"
-        >
-          Log Out
-        </button>
-      </>
-    ) : (
-      <></>
-    )}
-  </>
+        />
+      </RowComponent>
+    );
+  }
+  return <RowComponent showSpinner={true}>Authenticating...</RowComponent>;
 }
+
+const RowComponent = ({
+  children,
+  showSpinner,
+}: {
+  children: React.ReactNode;
+  showSpinner: boolean;
+}) => {
+  return (
+    <div className="row login-row">
+      <div className="card login-card">
+        <div className="inner">
+          <div className="grid cols-1 login-grid" style={{}}>
+            {showSpinner && <div className="spinner" />}
+            <div style={{ textAlign: 'center' }}>{children}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const LogIn = ({ styles }: { styles?: CSSProperties }) => {
+  const { openLogin } = useAuthUi();
+
+  return (
+    <button onClick={() => openLogin()} className="green" style={styles}>
+      Log In
+    </button>
+  );
+};
