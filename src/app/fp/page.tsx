ec4a60880aae6f3e23aba52740dc1d1110284f3e
phatln <phatln@undercurrent.tech>
2025-04-18 18:57:40 +0700

    new logic for login

--- src/app/fp/page.tsx
+++ src/app/fp/page.tsx
@@ -3,57 +3,41 @@
 import { useAuth } from '@futureverse/auth-react';
 import { CSSProperties, useEffect, useState } from 'react';
 import { useRouter } from "next/navigation";
-import { UserSession } from '@futureverse/auth-react/auth';
 import { useAuthUi } from '@futureverse/auth-ui';
 
 export default function FPPage() {
-  const { authClient } = useAuth();
-  const [signInState, setSignInState] = useState<boolean | undefined>(
-    undefined
-  );
+  const { authClient, userSession } = useAuth();
 
   const router = useRouter();
 
   useEffect(() => {
-    const userStateChange = (user: UserSession | undefined) => {
-      if (user) {
-        setSignInState(true);
-        router.push('/');
-      }
-      if (!user) {
-        setSignInState(false);
-      }
-    };
+    if (userSession) {
+      router.push('/fp');
+    }
+  }, [authClient, userSession]);
 
-    authClient.addUserStateListener(userStateChange);
-    return () => {
-      authClient.removeUserStateListener(userStateChange);
-    };
-  }, [authClient, router]);
-
-  if (signInState === true) {
-    return (
-      <RowComponent showSpinner={true}>
-        Redirecting, please wait...
-      </RowComponent>
-    );
-  }
-
-  if (signInState === false) {
-    return (
-      <RowComponent showSpinner={false}>
-        <div>Not Authenticated - Please Log In...</div>
-        <LogIn
-          styles={{
-            padding: ' 16px',
-            fontWeight: '700',
-            fontSize: '1.2rem',
-          }}
-        />
-      </RowComponent>
-    );
-  }
-  return <RowComponent showSpinner={true}>Authenticating...</RowComponent>;
+  return <>
+    {userSession ?
+      <>
+        <RowComponent showSpinner={true}>
+          {JSON.stringify(userSession)}
+        </RowComponent>
+      </>
+      :
+      <>
+        <RowComponent showSpinner={false}>
+          <div>Not Authenticated - Please Log In...</div>
+          <LogIn
+            styles={{
+              padding: ' 16px',
+              fontWeight: '700',
+              fontSize: '1.2rem',
+            }}
+          />
+        </RowComponent>
+      </>
+    }
+  </>
 }
 
 const RowComponent = ({
