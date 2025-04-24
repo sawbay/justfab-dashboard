2cac37a8e1213244c2724dcf8167e78c8146b4ed
phatln <phatln@undercurrent.tech>
2025-04-24 14:24:32 +0700

    try fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 import type { PropsWithChildren } from 'react';
 import type { Metadata } from 'next';
 import { getLocale } from 'next-intl/server';
-import Providers from '@/components/providers/Providers';
-import { Root } from '@/components/root/Root';
 import { I18nProvider } from '@/core/i18n/provider';
 
 import '@telegram-apps/telegram-ui/dist/styles.css';
 import 'normalize.css/normalize.css';
 import './globals.css';
+import Providers from '@/components/providers/Providers';
+import { Root } from '@/components/root/Root';
 
 export const metadata: Metadata = {
   title: 'JustFAB',
