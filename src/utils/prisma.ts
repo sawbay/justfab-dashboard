eccba1d4c95fe88c72496d43370e2046a38d1bb8
phatln <phatln@undercurrent.tech>
2025-04-28 13:42:13 +0700

    fix tg id

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@/app/generated/prisma";
 import { TelegramUserData } from "@telegram-auth/server";
 
 const globalForPrisma = global as unknown as {
-	prisma: PrismaClient
+  prisma: PrismaClient
 }
 
 const prisma = globalForPrisma.prisma || new PrismaClient()
@@ -12,18 +12,18 @@ if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 export default prisma
 
 export async function createUserOrUpdate(user: TelegramUserData) {
-	return prisma.user.upsert({
-		where: {
-			id: user.id.toString(),
-		},
-		create: {
-			id: user.id.toString(),
-			name: user.first_name,
-			image: user.photo_url,
-		},
-		update: {
-			name: user.first_name,
-			image: user.photo_url,
-		},
-	});
+  return prisma.user.upsert({
+    where: {
+      tgId: user.id,
+    },
+    create: {
+      tgId: user.id,
+      name: user.first_name,
+      image: user.photo_url,
+    },
+    update: {
+      name: user.first_name,
+      image: user.photo_url,
+    },
+  });
 }
