0beaca9e25a8b23f026bd3db2a560d3ca9a46f16
phatln <phatln@undercurrent.tech>
2025-05-19 20:59:08 +0700

    seems fixed

--- worker/src/inventory/inventory.processor.ts
+++ worker/src/inventory/inventory.processor.ts
@@ -1,17 +1,19 @@
-import { Processor, Process } from '@nestjs/bullmq';
+import { Processor } from '@nestjs/bullmq';
 import { Job } from 'bullmq';
 import { InventoryService } from './inventory.service';
 import { Injectable } from '@nestjs/common';
+import { WorkerHost } from '@nestjs/bullmq';
 
 export const INVENTORY_QUEUE = 'inventory_queue';
 
 @Processor(INVENTORY_QUEUE)
 @Injectable()
-export class InventoryProcessor {
-  constructor(private readonly inventoryService: InventoryService) {}
+export class InventoryProcessor extends WorkerHost {
+  constructor(private readonly inventoryService: InventoryService) {
+    super();
+  }
 
-  @Process('reward_inventory_item')
-  async handleRewardInventoryItem(job: Job) {
+  async process(job: Job) {
     const { userId, items } = job.data;
     await this.inventoryService.rewardItems(userId, items);
   }
