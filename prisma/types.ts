c09f2021520fcea63e720a4b00fea83f06ce474c
phatln <phatln@undercurrent.tech>
2025-05-19 22:51:59 +0700

    implement reward welcoome chest

---
+++ prisma/types.ts
@@ -0,0 +1,2 @@
+export const WELCOME_CHEST_ITEM_TYPE = 'WELCOME_CHEST';
+export const KEY_ITEM_TYPE = 'KEY';
--- worker/src/inventory/inventory.service.ts
+++ worker/src/inventory/inventory.service.ts
@@ -1,9 +1,52 @@
 import { Injectable, Logger } from '@nestjs/common';
 import prisma from '../../../prisma/prisma';
+import { WELCOME_CHEST_ITEM_TYPE, KEY_ITEM_TYPE } from '../../../prisma/types';
 
 @Injectable()
 export class InventoryService {
   private readonly logger = new Logger(InventoryService.name);
+
+  async rewardWelcomeChest(userId: string) {
+    // Implement welcome chest reward
+    // every user should get a welcome chest once
+    // if they have already received the welcome chest, do not reward again
+    // else, reward the welcome chest and key
+
+    const welcomeChestMeta = await prisma.inventoryMetadata.findFirst({
+      where: { type: WELCOME_CHEST_ITEM_TYPE },
+    });
+    if (!welcomeChestMeta) {
+      this.logger.warn('No InventoryMetadata found for welcome_chest');
+      return;
+    }
+    const hasWelcomeChest = await prisma.inventoryItem.findFirst({
+      where: {
+        userId,
+        inventoryMetadataId: welcomeChestMeta.id,
+      },
+    });
+    if (hasWelcomeChest) {
+      this.logger.log(`User ${userId} already has a welcome chest`);
+      return;
+    }
+
+    const keyMeta = await prisma.inventoryMetadata.findFirst({
+      where: { type: KEY_ITEM_TYPE },
+    });
+    if (!keyMeta) {
+      this.logger.warn('No InventoryMetadata found for key');
+      return;
+    }
+
+    // Reward both welcome chest and key
+    await prisma.inventoryItem.createMany({
+      data: [
+        { userId, inventoryMetadataId: welcomeChestMeta.id },
+        { userId, inventoryMetadataId: keyMeta.id },
+      ],
+    });
+    this.logger.log(`Rewarded welcome chest and key to user ${userId}`);
+  }
 
   async rewardItems(userId: string, items: string[]) {
     this.logger.log(`Rewarding items ${items} to user ${userId}`);
@@ -37,4 +80,4 @@ export class InventoryService {
       this.logger.log(`Rewarded item ${itemName} to user ${userId}`);
     }
   }
-} 
+}
