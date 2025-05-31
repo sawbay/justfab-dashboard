b82d3bdc7054931482599bf68b39b986657aeb26
phatln <phatln@undercurrent.tech>
2025-05-31 23:22:36 +0700

    use chest

--- worker/src/queue/queues/welcome-chest.processor.ts
+++ worker/src/queue/queues/welcome-chest.processor.ts
@@ -1,8 +1,12 @@
 import { Processor } from '@nestjs/bullmq';
 import { Job } from 'bullmq';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { WorkerHost } from '@nestjs/bullmq';
-import { Client, Databases } from 'node-appwrite';
+import { Client, Databases, ID, Permission, Query, Role } from 'node-appwrite';
+import getClient from 'src/lib/appwrite/server';
+import { DATABASE_ID, INVENTORY_COL_ID } from 'src/lib/appwrite/const';
+import { ItemType } from 'src/lib/types';
+import { WelcomeChestEventPayload } from './types';
 
 export const WELCOME_CHEST_QUEUE = 'welcome_chest_queue';
 
@@ -10,18 +14,61 @@ export const WELCOME_CHEST_QUEUE = 'welcome_chest_queue';
 @Injectable()
 export class WelcomeChestProcessor extends WorkerHost {
   private readonly client: Client;
+
   constructor() {
     super();
-    this.client = new Client()
-      .setEndpoint("https://fra.cloud.appwrite.io/v1")
-      .setProject("6836b47300371c4062a3")
-      .setKey("standard_feb203cc89624b58f97534f1e31637d2c953c9aae3fde8c40ce683640778245154a6260347b0f956a3bf8dcf1d30f7b2f78580fca65d3fb72baa070244bae7146cfb8e06fe4ac3c56ea2421930d149f03d58ca51b737a9abb480f155997f025993cfd36811e43caa68531b78701c6969ebd48e09b217ad85d0fbce880dfcd65d");
+    this.client = getClient();
   }
 
   async process(job: Job) {
-    // const { userId, items } = job.data;
-    // await this.inventoryService.rewardItems(userId, items);
+    const data: WelcomeChestEventPayload = job.data;
     const databases = new Databases(this.client);
-    databases.listDocuments
+    const existingWelcomeChest = await databases.listDocuments(
+      DATABASE_ID,
+      INVENTORY_COL_ID,
+      [
+        Query.equal("userId", data.userId),
+        Query.equal("itemType", ItemType.WELCOME_CHEST)
+      ]);
+
+    if (existingWelcomeChest.total > 0) {
+      job.log(`User ${data.userId} already rewarded welcome chest`);
+      return; // already rewarded welcome chest
+    }
+
+    try {
+      await databases.createDocument(
+        DATABASE_ID,
+        INVENTORY_COL_ID,
+        ID.unique(),
+        {
+          userId: data.userId,
+          itemType: ItemType.WELCOME_CHEST,
+          used: false,
+        },
+        [
+          Permission.read(Role.any()),
+        ]
+      );
+      job.log(`User ${data.userId} has been rewarded welcome chest`);
+
+      await databases.createDocument(
+        DATABASE_ID,
+        INVENTORY_COL_ID,
+        ID.unique(),
+        {
+          userId: data.userId,
+          itemType: ItemType.AURA_KEY,
+          used: false,
+        },
+        [
+          Permission.read(Role.any()),
+        ]
+      );
+      job.log(`User ${data.userId} has been rewarded aura key`);
+    } catch (error) {
+      job.log(`Error rewarding welcome chest to user ${data.userId}: ${error}`);
+      throw error;
+    }
   }
 }
