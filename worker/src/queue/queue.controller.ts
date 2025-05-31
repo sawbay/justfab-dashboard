9e4bf6468675225c5f27ab13c8a43a0e7e566215
phatln <phatln@undercurrent.tech>
2025-05-31 22:36:19 +0700

    fix

--- worker/src/queue/queue.controller.ts
+++ worker/src/queue/queue.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Post, Body, Logger } from '@nestjs/common';
 import { InjectQueue } from '@nestjs/bullmq';
 import { Queue } from 'bullmq';
 import { WELCOME_CHEST_QUEUE } from './queues/welcome-chest.processor';
-import { WorkerEvent } from './queue';
+import { WorkerEvent, WorkerEventType } from './queue';
 
 @Controller('queue')
 export class QueueController {
@@ -19,14 +19,24 @@ export class QueueController {
     @Body() body: { event: WorkerEvent }
   ) {
     const { event } = body;
-    const jobName = `${WELCOME_CHEST_QUEUE}_${typeof event.etype}`;
     try {
-      await this.welcomeChestQueue.add(jobName, { event });
-      this.logger.log(`Successfully enqueued job '${jobName}' with event: ${JSON.stringify(event)}`);
+      await this.routeEvent(event);
+      this.logger.log(`Enqueued event: ${JSON.stringify(event)}`);
       return { success: true };
     } catch (error) {
-      this.logger.error(`Failed to enqueue job '${jobName}': ${error.message}`, error.stack);
+      this.logger.error(`Failed to enqueue  event: ${JSON.stringify(event)}: ${error.message}`, error.stack);
       return { success: false, error: error };
+    }
+  }
+
+  async routeEvent(event: WorkerEvent) {
+    switch (event.etype) {
+      case WorkerEventType.REWARD_WELCOME_CHEST:
+        await this.welcomeChestQueue.add(WELCOME_CHEST_QUEUE, event.payload);
+        break;
+      default:
+        this.logger.error(`Unknown event type: ${event.etype}`);
+        break;
     }
   }
 } 
