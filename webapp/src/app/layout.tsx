ad93f48144fb9ed454e35e0b6fc34673b1c3c83b
phatln <phatln@undercurrent.tech>
2025-05-31 14:35:16 +0700

    fix

--- webapp/src/app/layout.tsx
+++ webapp/src/app/layout.tsx
@@ -1,19 +1,17 @@
+import "./globals.css";
+import "normalize.css/normalize.css";
+
 import type { PropsWithChildren } from "react";
 import { getLocale } from "next-intl/server";
 import { I18nProvider } from "@/core/i18n/provider";
 import AuthProvider from "@/components/providers/AuthProvider";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 import Providers from "@/components/providers/FvProvider";
 import { createMetadata } from "@/constants/metadata";
-import "normalize.css/normalize.css";
-import "./globals.css";
 
 export const metadata = createMetadata("Dashboard");
 
 export default async function RootLayout({ children }: PropsWithChildren) {
   const locale = await getLocale();
-  const session = await getServerSession(authOptions);
 
   return (
     <html lang={locale}>
