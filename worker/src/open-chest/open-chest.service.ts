a4a89836bab2f2a286daacacd48d69e8656aae57
phatln <phatln@undercurrent.tech>
2025-06-10 20:48:56 +0700

    update apply reward

--- worker/src/open-chest/open-chest.service.ts
+++ worker/src/open-chest/open-chest.service.ts
@@ -100,9 +100,8 @@ export class OpenChestService implements OnApplicationBootstrap {
 
   async openChest(userId: string, chestId: string): Promise<RewardType> {
     try {
-
+      const { keyId } = await this.checkChestAndKey(userId, chestId);
       const reward = await this.mutex.runExclusive(async () => {
-        const { keyId } = await this.checkChestAndKey(userId, chestId);
         const reward = await this.randomReward();
         await this.updateRewardState(reward);
         await this.applyReward(userId, chestId, keyId, reward);
