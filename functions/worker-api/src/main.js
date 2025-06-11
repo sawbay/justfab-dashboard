fe5700608521e7f35e3f5eeb493557d04302a6ba
phatln <phatln@undercurrent.tech>
2025-06-11 22:16:11 +0700

    wanna

--- functions/worker-api/src/main.js
+++ functions/worker-api/src/main.js
@@ -21,7 +21,7 @@ export default async ({ req, res, log, error }) => {
     });
 
     const data = await fetchRes.json().catch(() => null);
-    log(data);
+    log(await fetchRes.text());
 
     if (fetchRes.ok) {
       return res.json({
