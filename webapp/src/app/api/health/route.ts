35d9fe82152d7c9f356d286df18e76b6702af909
phatln <phatln@undercurrent.tech>
2025-06-11 21:14:10 +0700

    fix health

--- webapp/src/app/api/health/route.ts
+++ webapp/src/app/api/health/route.ts
@@ -1,14 +1,5 @@
-import { worker } from "@/utils/worker";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  let isWorkerHealthy = false;
-  try {
-    const data = await worker.health();
-    isWorkerHealthy = data.data.isWorkerHealthy;
-  } catch (error) {
-    isWorkerHealthy = false;
-  }
-
-  return NextResponse.json({ isWebappHealthy: true, isWorkerHealthy });
+  return NextResponse.json({ isWebappHealthy: true });
 }
